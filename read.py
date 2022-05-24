66af418214ad82e972bc119a1ca9067424564f53
Chiu Ling <corinna930512@gmail.com>
2022-05-24 18:52:57 +0800

    add filtering function

--- read.py
+++ read.py
@@ -10,8 +10,16 @@ print('檔案讀取完了，總共有',len(data),'筆資料')
 
 print(data[0])
 print('------------')
-
+#算每一個留言的平均字數
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print(sum_len / len(data))
+
+#清單篩選
+new = []
+for d in data:
+ 	if len(d) < 100:
+ 		new.append(d)
+print('一共有',len(new),'留言長度小於一百')
+print(new[0])
